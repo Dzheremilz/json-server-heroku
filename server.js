5aa3bb9e7308326d0722780925b76dddf375f595
Dzheremilz <trempejeremie@gmail.com>
2021-05-31 12:05:10 +0200

    passworded

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000;
 
+const isAuthorized = (req) => {
+  const method = ["POST", "PUT", "PATCH", "DELETE"];
+  if (
+    method.includes(req.method) &&
+    req.headers.authorization !== "Bearer 987654321"
+  ) {
+    return false;
+  }
+  return true;
+};
+
 server.use(middlewares);
+server.use((req, res, next) => {
+  if (isAuthorized(req)) {
+    next(); // continue to JSON Server router
+  } else {
+    res.sendStatus(401);
+  }
+});
 server.use(router);
 
 server.listen(port);
